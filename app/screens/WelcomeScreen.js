39aa813fe9c307be1a796b54f8bbb1549a786cb5
Chuck Thompson <chuckthompson27@me.com>
2021-01-26 00:05:22 -0800

    Initial commit.

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import AppButton from "../components/AppButton";
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground 
+        blurRadius={10}
+        source={require("../assets/background.jpg")} 
+        style={styles.background}>
+            <View style={styles.logoContainer}>
+                <Image source={require("../assets/logo-red.png")} style={styles.logo}/>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" />
+                <AppButton title="Register" color="secondary" />
+            </View>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    buttonsContainer: {
+        width: "100%",
+        padding: 20,
+    },
+    logo: {
+        height: 100,
+        width: 100,
+    },
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        alignItems: "center",
+    },
+    tagline: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20
+    }
+});
+
+export default WelcomeScreen;
